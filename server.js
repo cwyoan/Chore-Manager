2c7a3b577383be51a78b3f47381f9985831ebcec
Justin Berke <52087129+JustinBerke0406@users.noreply.github.com>
2025-02-27 04:16:59 -0500

    Added friend system

--- server.js
+++ server.js
@@ -17,6 +17,13 @@ class Server {
   }
 
   /**
+   * Close the server connection.
+   */
+  close() {
+    this.#connection.end();
+  }
+
+  /**
    * Attempts to connect to the server and return its status.
    * @returns An error message or "Connected".
    */
@@ -26,6 +33,8 @@ class Server {
         if (err) return reject(err);
         return resolve("Connected");
       });
+    }).catch((error) => {
+      return error;
     });
   }
 
@@ -36,7 +45,13 @@ class Server {
    * @async
    */
   async getUser(userID) {
-    return await this.query("select * from users where UserID = ?", [userID]);
+    var user = await this.query("select * from users where UserID = ?", [
+      userID,
+    ]);
+
+    if (user.length == 0) return null;
+
+    return user[0];
   }
 
   /**
@@ -49,22 +64,166 @@ class Server {
     if (!("UserID" in params)) return false;
 
     var id = params["UserID"];
-    var allKeys = Object.keys(params);
-    var vals = Object.values(params);
 
-    if ((await this.getUser(id)).length != 0) {
-      var sets = [];
+    if (await this.getUser(id))
+      return (
+        await this.#tryQuery("update users set ? where UserID = ?", [
+          params,
+          id,
+        ])
+      )[0];
 
-      for (var keys in params) sets.push(keys + " = ?");
+    return (await this.#tryQuery("insert into users set ?", [params]))[0];
+  }
 
-      var msg = `update users set ${sets} where UserID = ?`;
-      vals.push(id);
+  /**
+   * Returns a list of a user's friends.
+   * @param {number} userID The ID of the user.
+   * @returns A list of user dictionaries.
+   * @async
+   */
+  async getFriends(userID) {
+    return await this.query(
+      "select * from users where users.UserID in ((select UserID from friends where FriendID = ? and Status = 'ACCPT') union (select FriendID from friends where UserID = ? and Status = 'ACCPT'))",
+      [userID, userID]
+    );
+  }
 
-      return (await this.#tryQuery(msg, vals))[0];
-    }
+  /**
+   * Returns a list of a user's friends, including themselves, ordered by score.
+   * @param {number} userID The ID of the user.
+   * @returns A list of user dictionaries.
+   * @async
+   */
+  async getFriendsRanked(userID) {
+    return await this.query(
+      "select * from users where users.UserID in ((select UserID from friends where FriendID = ? and Status = 'ACCPT') union (select FriendID from friends where UserID = ? and Status = 'ACCPT') union (select UserID from users where UserID = ?)) order by score desc",
+      [userID, userID, userID]
+    );
+  }
+
+  /**
+   * Returns the friend status between two users.
+   * @param {number} userID1
+   * @param {number} userID2
+   * @return {string} REQ_0, REQ_1, BLK_0, BLK_1, ACCPT, EMPTY.
+   * @async
+   */
+  async getFriendStatus(userID1, userID2) {
+    var result = await this.query(
+      "select Status from friends where UserID = ? and FriendID = ?",
+      [Math.min(userID1, userID2), Math.max(userID1, userID2)]
+    );
+
+    if (result.length == 0) return "EMPTY";
+
+    return result[0]["Status"];
+  }
+
+  /**
+   * Send a friend request between two users.
+   * @param {number} userID1 The user sending the request.
+   * @param {number} userID2 The user receiving the request.
+   * @return {boolean} Whether the request was successfully sent.
+   * @async
+   */
+  async sendFriendRequest(userID1, userID2) {
+    if (userID1 == userID2) return false;
+
+    var status = await this.getFriendStatus(userID1, userID2);
+
+    if (status == "BLK_0" || status == "BLK_1" || status == "ACCPT")
+      return false;
+
+    var reqType = "REQ_0";
+
+    if (userID1 > userID2) reqType = "REQ_1";
+
+    if (status == reqType) return true;
+
+    if (status == "EMPTY")
+      return (
+        await this.#tryQuery("insert into friends values (?, ?, ?)", [
+          Math.min(userID1, userID2),
+          Math.max(userID1, userID2),
+          reqType,
+        ])
+      )[0];
+
+    return await this.acceptFriendRequest(userID1, userID2);
+  }
+
+  /**
+   * Block a user as another user.
+   * @param {number} userID1 The user sending the block.
+   * @param {number} userID2 The user receiving the block.
+   * @return {boolean} Whether the block was successfully sent.
+   * @async
+   */
+  async sendBlockRequest(userID1, userID2) {
+    if (userID1 == userID2) return false;
+
+    var status = await this.getFriendStatus(userID1, userID2);
+
+    var reqType = "BLK_0";
+
+    if (userID1 > userID2) reqType = "BLK_1";
+
+    if (status == "Empty")
+      return (
+        await this.#tryQuery("insert into friends values (?, ?, ?)", [
+          Math.min(userID1, userID2),
+          Math.max(userID1, userID2),
+          reqType,
+        ])
+      )[0];
 
     return (
-      await this.#tryQuery("insert into users ? values ?", [[allKeys], [vals]])
+      await this.#tryQuery(
+        "update friends set Status = ? where UserID = ? and FriendID = ?",
+        [reqType, Math.min(userID1, userID2), Math.max(userID1, userID2)]
+      )
+    )[0];
+  }
+
+  /**
+   * Accept a friend request.
+   * @param {number} userID1 The user accepting the request.
+   * @param {number} userID2 The user sending the request.
+   * @returns {boolean} Whether the request was successfully accepted.
+   * @async
+   */
+  async acceptFriendRequest(userID1, userID2) {
+    if (userID1 == userID2) return false;
+
+    var status = await this.getFriendStatus(userID1, userID2);
+    var reqType = "REQ_1";
+
+    if (userID1 > userID2) reqType = "REQ_0";
+
+    if (status != reqType) return false;
+
+    return (
+      await this.#tryQuery(
+        "update friends set Status = ? where UserID = ? and FriendID = ?",
+        ["ACCPT", Math.min(userID1, userID2), Math.max(userID1, userID2)]
+      )
+    )[0];
+  }
+
+  /**
+   * Clears the friend status between two users.
+   * @param {number} userID1
+   * @param {number} userID2
+   * @returns Whether the friend status was successfully cleared.
+   * @async
+   */
+  async clearFriendStatus(userID1, userID2) {
+    return (
+      await this.#tryQuery(
+        "delete from friends where UserID = ? and FriendID = ?",
+        [Math.min(userID1, userID2), Math.max(userID1, userID2)]
+      )
     )[0];
   }
 
@@ -89,21 +248,27 @@ class Server {
    * Attempts to send a query and parse the results.
    * @param {string} sql Query message.
    * @param {list} parameters An optional list of values to fill placeholders.
-   * @returns {Dictionary|null} A dictionary of a query's returned information, or *null* if the query failed.
+   * @returns {List<Dictionary>} A list of dictionaries of returned row data, or an empty list if the query failed.
    * @async
    */
   async query(sql, parameters = []) {
     var result = await this.#tryQuery(sql, parameters);
 
-    if (!result[0]) return null;
+    if (!result[0]) return [];
+
+    var data;
+    var dictList = [];
 
     result = result[1];
 
-    var data = {};
+    for (var i in result) {
+      data = {};
 
-    for (var col in result[0]) data[col] = result[0][col];
+      for (var col in result[i]) data[col] = result[i][col];
+      dictList.push(data);
+    }
 
-    return data;
+    return dictList;
   }
 }
 
