6ba98dba683793cbd69f7782f396b7bb14f23ac8
Justin Berke <justinberke0406@gmail.com>
2025-03-04 02:48:18 -0500

    created frontend backend connection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Connector from "../../backend/connector";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  // Backend connection (must be in async function)
+  //var conn = new Connector();
+
+  useEffect(() => {}, []);
 
   return (
     <>
@@ -29,7 +35,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
